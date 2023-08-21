02d12520dda2fd39cb56c6a037eab3841c93b203
tcxiao <815570213@qq.com>
2023-08-21 10:31:34 +0800

    feat:add 203

---
+++ src/main/java/cn/tc/list/List203.java
@@ -0,0 +1,81 @@
+package cn.tc.list;
+
+public class List203 {
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(6);
+        ListNode node4 = new ListNode(3);
+        ListNode node5 = new ListNode(4);
+        ListNode node6 = new ListNode(5);
+        ListNode node7 = new ListNode(6);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+
+        System.out.println(removeElements(node1, 6));
+
+    }
+
+    public static ListNode removeElements(ListNode head, int val) {
+        ListNode tmp = new ListNode();
+        tmp.next = head;
+
+        ListNode pre = tmp;
+
+        while (head != null) {
+
+            if (head.val == val) {
+                pre.next = head.next;
+            } else {
+                pre = head;
+            }
+
+            head = head.next;
+        }
+
+        return tmp.next;
+    }
+}
+
+//给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [1,2,6,3,4,5,6], val = 6
+//输出：[1,2,3,4,5]
+//
+//
+// 示例 2：
+//
+//
+//输入：head = [], val = 1
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：head = [7,7,7,7], val = 7
+//输出：[]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 列表中的节点数目在范围 [0, 10⁴] 内
+// 1 <= Node.val <= 50
+// 0 <= val <= 50
+//
+//
+// Related Topics 递归 链表 👍 1309 👎 0
+
